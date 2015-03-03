91f261e84cadc60ea0aeb410c6d7278f5aa991c7
superfan <bicasi@sina.com>
2015-03-03 15:25:26 +0800

    add the function of http

--- assets/www/js/global.js
+++ assets/www/js/global.js
@@ -4,5 +4,24 @@ var myGlobal = {
         if (reg.test(location.href)) return unescape(RegExp.$2.replace(/\+/g, " ")); return "";
     },
 
-    db : "file:///mnt/sdcard/sh3h_html/meterreading/data/main.cbj"
+    db : "file:///mnt/sdcard/sh3h_html/meterreading/data/main.cbj",
+
+    id : 10000,
+
+    baseUrl : "http://128.1.2.106:8084/", //http://128.1.3.186:8084
+
+    login : function(http, account, password, success, error) {
+        var request = {
+            method: 'POST',
+            url: this.baseUrl + '/user.ashx',
+            headers: {
+                'Content-Type': 'text/plain; charset=utf-8',
+                'X-JSON-RPC': 'Login'
+            },
+            data: '{\"id\":' + (this.id++) + ',\"method\":\"' + 'Login' + '\",\"params\":'
+                    + '{ "loginfo" : {"Account":"' + account + '","VerType":"Simple","Password":"' + password + '"} } }'
+        };
+
+        http(request).success(success).error(error);
+    }
 };
--- assets/www/js/login.js
+++ assets/www/js/login.js
@@ -1,5 +1,5 @@
 angular.module('login', ['ionic'])
-.controller('LoginCtrl', function($scope) {
+.controller('LoginCtrl', function($scope, $http) {
     var isOpened = false;
 
     $scope.user = {
@@ -8,7 +8,31 @@ angular.module('login', ['ionic'])
     };
 
     $scope.onLogin = function() {
-        if (isOpened) {
+        function success(response) {
+            if (response.result.success) {
+                location.href = "main.html?account=" + $scope.user.name;
+            }
+            else {
+                alert("name or password is not correct!!!");
+            }
+        }
+
+        function error(response) {
+            alert("name or password is not correct!!!");
+        }
+
+        myGlobal.login($http, $scope.user.name, $scope.user.password, success, error);
+
+
+        /*var request = {
+            id : 12345,
+            method : 'Login',
+            params : '{ "loginfo" : {"Account":"2134","VerType":"Simple","Password":"0000"} }'
+        };
+
+        callSync(request);*/
+
+        /*if (isOpened) {
             var sql = "select * from user where Account = '"
                         +  $scope.user.name + "' and PassWord = '" + $scope.user.password + "'";
             window.cordovaSQLite.execQuerySingleResult(sql,
@@ -27,39 +51,33 @@ angular.module('login', ['ionic'])
         }
         else {
             alert("db is not opened!!!");
-        }
-
-        //alert("1234");
-        /*if (window.loginModel.onLogin($scope.user.name, $scope.user.password)) {
-            alert("success");
-            location.href = "main.html";
-        }
-        else {
-            alert("failure");
         }*/
-        /*mydb.query("CREATE TABLE user (userName TEXT(50), PassWord TEXT(50), Account TEXT(50))",
-            function(ret) {
-                        if (ret.length == 0) {
-                            alert("1111");
-                        }
-                        else {
-                            alert("2222");
-                        }
-                    });*/
-        /*var sql = "select * from user where Account = "
-            +  $scope.user.name + " and PassWord = " + $scope.user.password;
-        mydb.fetchAll(sql, function(ret) {
-            if (ret.length == 0) {
-                alert("1111");
-            }
-            else {
-                alert("2222");
-            }
-        });*/
-        //mydb.checkUser($scope.user.name, $scope.user.password);
     };
 
-    function onDeviceReady() {
+    function callSync(request) {
+        /*$http.get("http://www.w3cschool.cc/try/angularjs/data/Customers_JSON.php")
+            .success(function(response) {
+            $scope.names = response;
+        });*/
+        var http = window.XMLHttpRequest ?
+            new XMLHttpRequest() :
+            new ActiveXObject('Microsoft.XMLHTTP');
+        http.open('POST', 'http://128.1.3.186:8084/user.ashx', false);
+        http.setRequestHeader('Content-Type', 'text/plain; charset=utf-8');
+        http.setRequestHeader('X-JSON-RPC', request.method);
+        //http.setRequestHeader('Access-Control-Allow-Origin', '*');
+        http.send('{\"id\":' + request.id + ',\"method\":\"' + request.method + '\",\"params\":' + request.params + '}');
+        if (http.status != 200) {
+            throw { message : http.status + ' ' + http.statusText, toString : function() { return this.message; } };
+        }
+        var clockStart = new Date();
+        var response = JSON.parse(http.responseText);
+        response.timeTaken = (new Date()) - clockStart;
+        response.http = { text : http.responseText, headers : http.getAllResponseHeaders() };
+        return response;
+    }
+
+    /*function onDeviceReady() {
         window.cordovaSQLite.openDatabase(myGlobal.db, 0,
             function() {
                 isOpened = true;
@@ -69,5 +87,5 @@ angular.module('login', ['ionic'])
             });
     }
 
-    document.addEventListener("deviceready", onDeviceReady, false);
+    document.addEventListener("deviceready", onDeviceReady, false);*/
 });
